661dae78cb27be59d6e3521f27c0500394ed6ab3
ian-ramos <ramos.ianc@gmail.com>
2017-08-30 18:00:41 -0400

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).delete_if do |word|
+      word == "" #if a sentence ends in "!!", it will split on both, but the second ! will just show up as ""
+    end.count
   end
 end
